f2fd604f5553eecd95beae3274bcab1f5c285b9b
Shuan772 <alexanderschultes@t-online.de>
2018-12-09 21:23:59 +0100

    Benutezr kram hinzugefügt

---
+++ Copy Ordner/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBWT_Paket_5.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // POST /Login/Index mit 
+        //return Inaktiv 
+        //return False 
+        //return True
+        //bei True session setzten
+
+        //POST Login/Logout
+        //return Index 
+
+
+        // GET /Login/Registrieren
+        // return Create View
+
+        //POST /Login/Registrieren
+        //back wenn eingaben falsch 
+        // wenn I.O. dann weiter zu Create 
+        //return Index mit erfolgreich registriert Acc muss noch aktiviert werden
+    }
+}
--- Copy Ordner/Models/Benutzer.cs
+++ Copy Ordner/Models/Benutzer.cs
@@ -18,15 +18,14 @@ namespace DBWT_Paket_5.Models
         public short Nummer { get; set; }
         [Required]
         public string E_Mail { get; set; }
-        /*
         public DateTime LetzterLogin { get; set; }
         [Required]
         public string Nutzername { get; set; }
-        public Date Geburtsdatum { get; set; }
+        public DateTime Geburtsdatum { get; set; }
         [Required]
         public bool Aktiv { get; set; }
         [Required]
-        public Date Anlegedatum  { get; set; }
+        public DateTime Anlegedatum { get; set; }
         public short Alter { get; set; }
         [Required]
         public string Vorname { get; set; }
@@ -36,14 +35,130 @@ namespace DBWT_Paket_5.Models
         public string Salt { get; set; }
         [Required]
         public string Hash { get; set; }
-
         [Required]
-        public ENUM("Gast" , "FH-Angehöriger") ISA { get; set; }
+        public string Rolle { get; set; }
         public string Grund { get; set; }
-        public Date Ablaufdatum { get; set; }
+        public DateTime Ablaufdatum { get; set; }
         private short Matrikelnummer { get; set; }
-        public ENUM("ET", "INF", "ISE", "MCD", "WI") Studiengang { get; set; }
-        */
+        public string Studiengang { get; set; }
+        public string Büro { get; set; }
+        public string Telefon { get; set; }
+
+        /*ENUM("Gast" , "FH-Angehöriger")
+         ENUM("ET", "INF", "ISE", "MCD", "WI")
+         */
+        public static Benutzer GetByNutzername(string Nutzername)
+        {
+            Benutzer m = new Benutzer();
+            using (MySqlConnection con =
+                new MySqlConnection(ConfigurationManager.ConnectionStrings["connString"].ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (MySqlCommand cmd = new MySqlCommand("", con))
+                    {
+                        cmd.CommandText = "SELECT Nummer, LetzterLogin, Nutzername, Aktiv, Vorname , Nachname , Salt , Hash ,ISA FROM Benutzer WHERE Nutzername=@mid ";
+                        cmd.Parameters.AddWithValue("mid", Nutzername);
+
+                        var r = cmd.ExecuteReader();
+                        if (r.Read())
+                        {
+                            m.Nummer = Int16.Parse(r["Nummer"].ToString());
+                            m.Vorname = r["Vorname"].ToString();
+                            m.Nachname = r["Nachname"].ToString();
+                            m.Nutzername = r["Nutzername"].ToString();
+                            m.LetzterLogin = Convert.ToDateTime(r["LetzterLogin"]);
+                            m.Aktiv = Convert.ToBoolean(r["Aktiv"]);
+                            m.Salt = r["Salt"].ToString();
+                            m.Hash = r["Hash"].ToString();
+                            m.Rolle = r["ISA"].ToString();
+                        }
+                    }
+                    con.Close();
+                    con.Open();
+                    using (MySqlCommand cmd = new MySqlCommand("", con))
+                    {
+                        if (m.Rolle == "Gast")
+                        {
+                            cmd.CommandText = "SELECT Grund,Ablaufdatum FROM `gäste` WHERE Nummer =@mid ";
+                        }
+                        else
+                        {
+                            cmd.CommandText = "SELECT s.id m.Telefon, m.`büro`, s.Studiengang  FROM Benutzer b LEFT JOIN Mitarbeiter m on m.id = b.nummer LEFT JOIN Student s on s.id = b.nummer WHERE Nummer =@mid ";
+                        }
+                        cmd.Parameters.AddWithValue("mid", m.Nummer);
+
+                        var r = cmd.ExecuteReader();
+                        if (r.Read())
+                        {
+                            if (m.Rolle == "Gast")
+                            {
+                                m.Grund = r["Grund"].ToString();
+                                m.Rolle = "Gast";
+                                m.Ablaufdatum = Convert.ToDateTime(r["Ablaufdatum"]);
+                            }
+                            else
+                            {
+                                if (String.IsNullOrEmpty(r["s.id"].ToString()))
+                                {
+                                    m.Rolle = "Mitarbeiter";
+                                    m.Büro = r["m.büro"].ToString();
+                                    m.Telefon = r["m.Telefon"].ToString();
+                                }
+                                else
+                                {
+                                    m.Rolle = "Student";
+                                    m.Studiengang = r["s.Studiengang"].ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    // Haltepunkt?
+                    string ex = e.Message;
+                }
+                // using schließt die Verbindung auch wieder ;)
+            }
+            return m;
+        }
+        public static string VerifyBenutzer(string pw, string Nutzername)
+        {
+            Benutzer m = new Benutzer();
+            m = Benutzer.GetByNutzername(Nutzername);
+
+            if (m.Aktiv)
+            {
+                bool testhash = PasswordSecurity.PasswordStorage.VerifyPassword(pw, "sha1:64000:18:" + m.Salt + ":" + m.Hash);
+                return testhash.ToString();
+            }
+            else
+            {
+                return "Account Inaktiv";
+            }
+
+            //Create 
+            //Bei Create Type String 
+            // muss grund zurück geben.
+            //string result = PasswordSecurity.PasswordStorage.CreateHash(reader["salt"].ToString() + Request.Form["password"]);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
-}
+    }
+
  
